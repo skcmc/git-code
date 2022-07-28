d518ae3b8422f9030c44ade2e843972fb364bc96
Jade <xiaoyu_shao@126.com>
2022-07-28 14:50:08 +0800

    first idea commit

---
+++ src/main/java/xiaoyu/shao/git/GitTest.java
@@ -0,0 +1,11 @@
+package xiaoyu.shao.git;
+
+/**
+ * @author sxy
+ * @create 2022-07-28 14:47
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
